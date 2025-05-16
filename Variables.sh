04e06f1ad95839f285d19242901a05275072b009
saivema9 <saiteja.vema1@gmail.com>
2025-05-16 13:05:15 +0530

    Third commit

--- Variables.sh
+++ Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$Name1 = $1
-$Name2 = $2
+Name1 = $1
+Name2 = $2
 
-echo $Name1 is good person
-echo $Name2 is bad person for $Name1
+echo Name1 is good person
+echo  Name2 is bad person for Name1
