426f9ddb674490805009c48fd6bff914c6c0e010
saivema9 <saiteja.vema1@gmail.com>
2025-05-16 21:59:55 +0530

    Special Variables commit

---
+++ functions01.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=(id -u)
+
+if [ $ID -ne 0 ]
+    then 
+        echo "Error:: please try with the sudo access"
+        exit 1
+    else 
+        echo "You are running with sudo access"
+fi
+
+validate(){
+
+if [ $1 -eq 0 ]
+    then 
+        echo "Installing $2 is success"
+    else
+        echo "Installing $2 is failure"
+        exit 1
+fi
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+    then 
+        echo "MySQL is not installed on this machine"
+        echo "Installing MYSQL...."
+        dnf install mysql -y
+        validate $? "MySQL"
+else
+        echo "My is already installed on this machine"
